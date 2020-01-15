97b5be268da0b62f87ba3282cf6f6992553da4ee
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2020-01-15 15:14:34 -0800

    edit query header limitations, second time

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
