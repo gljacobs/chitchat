39c414a150a153805e9eb79069828fa1e5c6b104
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2020-01-20 01:46:21 -0800

    remove uneeded wrapper comp

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import API from './utils/API';
 
-import Wrapper from "./components/Wrapper";
 import Navbar from './components/Navbar';
 import Login from './components/Login';
 import Signup from './components/Signup';
