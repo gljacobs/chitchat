d1bdec2be7eeeae03f8bace7665e258cb2452c1d
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2020-02-08 13:45:53 -0800

    add logo assets and fix

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './style.css';
+import cat from '../../mew.gif'
 
 function Navbar(props) {
     return (
         <nav id="navbar">
             <div className="nav-wrapper">
                 <a href="/">ChitChat
-                    <img src="https://images.narrative.org/1.0/images/54518734051519678/largesquare-Pixel-Cat.gif"/>
+                    <img src={cat}/>
                 </a>
                 <p>{props.user ? `Welcome ${props.user}` : "No user is currently logged in"}</p>
             </div>
